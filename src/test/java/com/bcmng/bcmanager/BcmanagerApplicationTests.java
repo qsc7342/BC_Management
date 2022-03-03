d9883feeba1146dd24438a4627e4b486284f792e
JoonHyukYang <jun97y@gmail.com>
2022-03-03 10:20:25 +0900

    Init Project
    initial commit

---
+++ src/test/java/com/bcmng/bcmanager/BcmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcmng.bcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
