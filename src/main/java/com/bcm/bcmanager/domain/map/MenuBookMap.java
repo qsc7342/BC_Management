ff0a2c915158bc62306080472cad9093856b0116
JoonHyukYang <jun97y@gmail.com>
2022-03-22 13:38:34 +0900

    다대다 관계로 수정

--- src/main/java/com/bcm/bcmanager/domain/map/MenuBookMap.java
+++ src/main/java/com/bcm/bcmanager/domain/map/MenuBookMap.java
@@ -1,13 +1,13 @@
 package com.bcm.bcmanager.domain.map;
 
+import com.bcm.bcmanager.domain.book.Book;
 import com.bcm.bcmanager.domain.menu.Menu;
-import com.bcm.bcmanager.domain.origin.Origin;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 
 @Entity
-public class MenuOriginMap {
+public class MenuBookMap {
 
     @Id
     @JsonIgnore
@@ -15,12 +15,12 @@ public class MenuOriginMap {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "origin_id")
-    private Origin origin;
+    @JoinColumn(name = "book_id")
+    @JsonIgnore
+    private Book book;
 
     @ManyToOne
     @JoinColumn(name = "menu_id")
     @JsonIgnore
     private Menu menu;
-
 }
