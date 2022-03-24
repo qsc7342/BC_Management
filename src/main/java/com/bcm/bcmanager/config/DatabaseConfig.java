b5bb6c80a0b8f17fb2da2c9312b45e7b464a4350
JoonHyukYang <jun97y@gmail.com>
2022-03-24 17:03:25 +0900

    환경 분리

---
+++ src/main/java/com/bcm/bcmanager/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.bcm.bcmanager.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile(value = "testDB")
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:testDB.db");
+        return dataSourceBuilder.build();
+    }
+}
